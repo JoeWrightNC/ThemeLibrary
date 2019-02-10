89ed2ad5c4fb8045d5b08545c51dbdd62b52d3fe
Joe Wright <joewrightart@gmail.com>
2019-02-10 17:48:02 -0500

    just need to build out more json data

--- src/components/mainApp/mainApp.js
+++ src/components/mainApp/mainApp.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 import {CopyToClipboard} from 'react-copy-to-clipboard';
 import paintings from "../../paintings.json";
 import "./mainApp.css";
-import menuIcon from "../../images/threelines.png"
+import menuIcon from "../../images/threelines.png";
+import $ from 'jquery';
 class MainApp extends Component {
 
   state = {
     paintings,
-    message: "Click on any color card below to copy the palette, declared as SCSS variables, to your clipboard! ",
+    message: "Click on any color card copy button to copy the palette, declared as SCSS variables, to your clipboard! ",
     copied: false,
   };
 
@@ -40,6 +41,12 @@ class MainApp extends Component {
     document.getElementById("mySidenav").style.width = "0";
   }
 
+  randomizer = () => {
+    var min = 0;
+    var max = 3;
+    var rand =  min + (Math.random() * (max-min));
+    return rand
+  }
   shufflePalettes = () => {
     const paintingsArray = this.state.paintings;
     return this.setState({
@@ -48,6 +55,12 @@ class MainApp extends Component {
     })
   }
 
+  filterDOM = (filter) => {
+    console.log(filter)
+    const filterShow = `.${filter}`;
+    $(".paletteCard").hide();
+    $(filterShow).show()
+  }
   
   render() {
     return (
@@ -55,11 +68,12 @@ class MainApp extends Component {
       <section>
         <div id="mySidenav" className="sidenav">
           <a href="javascript:void(0)" className="closebtn" onClick={this.closeNav}>&times;</a>
-          <h4>Filters:</h4>
-          <a href="#">About</a>
-          <a href="#">Services</a>
-          <a href="#">Clients</a>
-          <a href="#">Contact</a>
+          <h4>Filter By:</h4>
+          <span onClick={() => this.filterDOM("warm")}>Warm Tones</span>
+          <span onClick={() => this.filterDOM("cool")}>Cool Tones</span>
+          <span onClick={() => this.filterDOM("hued")}>Hued Variants</span>
+          <span onClick={() => this.filterDOM("multicolor")}>Multicolor</span>
+          <span onClick={() => this.filterDOM("paletteCard")}>Show All</span>
         </div>
 
         <span onClick={this.openNav} className="collapsedMenu"><img className="menuIcon" src={menuIcon} alt="View Menu" /></span>
@@ -73,18 +87,19 @@ class MainApp extends Component {
             
             <div className="row">
             {this.state.paintings.map(palette => (
-              <div className="paletteCard col-10 col-md-6 col-xl-4 my-4 mx-auto" key={palette.id} onClick={this.copyToClipboard}>
+              <div className={`paletteCard col-10 col-md-6 col-xl-4 my-4 mx-auto ${palette.classList}`} key={palette.id} onClick={this.copyToClipboard}>
                 <div className="paletteCardInside">
-                  <h5 id={palette.name} className="cardHeader mb-0">{palette.name}</h5> 
+                  <h5 style={{backgroundImage: "linear-gradient(to right," + palette.colorArr[Math.floor(Math.random()*2)] + ", " + palette.colorArr[Math.floor(Math.random()*2)+2] + ")"}} id={palette.name} className="cardHeader mb-0 py-3">{palette.name}
+                    <br/>
+                    <CopyToClipboard text={palette.clipBoard}
+                      onCopy={() => this.setState({message: 'Palette SCSS Variables Copied to Clipboard'})}>
+                      <button className="mt-3 copyBtn">Copy <i className="far fa-clipboard"></i></button>
+                    </CopyToClipboard>
+                  </h5> 
                   {palette.colorArr.map(color => {
                     const colorBarStyle = {backgroundColor: color};
-                    return <div className="colorBar" style={colorBarStyle} key={color}><p className="colorText">{color}</p></div>
+                    return <div className="colorBar" style={colorBarStyle} key={color}><p className="colorText px-2">{color}</p></div>
                   })}
-                  <CopyToClipboard text={palette.clipBoard}
-                    onCopy={() => this.setState({message: 'Palette SCSS Variables Copied to Clipboard'})}>
-                    <button className="my-3 copyBtn">Copy <i class="far fa-clipboard"></i></button>
-                  </CopyToClipboard>
-                  
                 </div>
               </div>
             ))}
